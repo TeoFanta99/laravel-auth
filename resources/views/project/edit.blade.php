807fff3cb97acd1e967f96ea24621be8ee667593
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-02-21 14:18:27 +0100

    add welcome e edit style

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
+
 @section('content')
+    <div class="container-form">
+        <form action="{{ route('project.update', $project->id) }}" method="POST" class="card-form">
+            @csrf
+            @method('PUT')
 
-<h1>EDIT</h1>
+            <div class="form-group">
+                <label for="name">Nome:</label>
+                <input type="text" name="name" id="name" class="form-control" value="{{ $project->name }}">
+            </div>
 
-<form action="{{route('project.update', $project -> id)}}" method="POST">
+            <div class="form-group">
+                <label for="description">Descrizione:</label>
+                <input type="text" name="description" id="description" class="form-control"
+                    value="{{ $project->description }}">
+            </div>
 
-    @csrf
-    @method('PUT')
+            <div class="form-group">
+                <label for="author">Autore:</label>
+                <input type="text" name="author" id="author" class="form-control" value="{{ $project->author }}">
+            </div>
 
-    <label for="name">Nome: </label>
-    <input type="text" name="name" id="name" value="{{$project -> name}}">
-    <br>
-    <label for="description">Descrizione: </label>
-    <input type="text" name="description" id="description" value="{{$project -> description}}">
-    <br>
-    <label for="author">Autore: </label>
-    <input type="text" name="author" id="author" value="{{$project -> author}}">
-    <br>
-    <input type="submit" value="UPDATE">
-</form>
-
+            <div class="text-center">
+                <input type="submit" value="UPDATE" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
 @endsection
--- resources/views/project/welcome.blade.php
+++ resources/views/project/welcome.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.app')
+
 @section('content')
+    <div class="container mt-5">
+        <h1 class="text-center">PROGETTI</h1>
+        <div class="text-center mb-3">
+            <a href="{{ route('project.create') }}" class="btn btn-primary">ADD NEW</a>
+        </div>
 
-<h1>PROGETTI</h1>
-<br>
-<button>
-    <a href="{{route('project.create')}}">ADD NEW</a>
-</button>
-<br>
-<ul>
-    @foreach ($projects as $project)
-    <li>
-        <a href="{{route('project.show', $project -> id)}}">
-            {{$project -> name}}
-        </a>
-        @auth
-        <a href="{{route('project.edit', $project -> id)}}" class="mx-3">EDIT</a>
-        <form action="{{route('project.delete', $project -> id)}}" method="POST" class="d-inline-block">
+        <div class="card-deck">
+            @foreach ($projects as $project)
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="{{ route('project.show', $project->id) }}">
+                                {{ $project->name }}
+                            </a>
+                        </h5>
+                        @auth
+                            <a href="{{ route('project.edit', $project->id) }}" class="btn btn-info">EDIT</a>
+                            <form action="{{ route('project.delete', $project->id) }}" method="POST" class="d-inline-block">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="DELETE" class="btn btn-danger">
+                            </form>
+                        @endauth
+                    </div>
+                </div>
+            @endforeach
+        </div>
 
-            @csrf
-            @method('DELETE')
-
-            <input type="submit" value="DELETE">
-        </form>
-        @endauth
-        
-    </li>
-    @endforeach
-   
-</ul>
-
+    </div>
 @endsection
